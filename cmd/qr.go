4ce5bd810f766debf79d11035380066bb4a2de1a
Francisco Ignacio Cueto <francue3@gmail.com>
2025-12-08 23:24:32 -0300

    el docker anda mas o menos

--- cmd/qr.go
+++ cmd/qr.go
@@ -37,7 +37,7 @@ func generateQRCode(cmd *cobra.Command, args []string) {
 		panic(fmt.Errorf("failed to scale QR code: %w", err))
 	}
 
-	file, err := os.Create("qrcode.png")
+	file, err := os.Create("output/qrcode.png")
 	if err != nil {
 		panic(fmt.Errorf("failed to create file: %w", err))
 	}
