8f4d18eab51171aed5a1f4efcdd71b7b92987b92
francisco <francue3@gmail.com>
2025-12-13 13:44:47 -0300

    deje igual el server

--- server/handlers/pdfHandler.go
+++ server/handlers/pdfHandler.go
@@ -1,9 +1,23 @@
 package handlers
 
 import (
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
 func HandlePDFs(r *gin.Engine) {
+	r.GET("/pdf/:tokenID", func(c *gin.Context) {
 
+		tokenID := c.Param("tokenID")
+
+		controller := c.MustGet("tokenController").(*TokenController)
+		token, err := controller.GetToken(tokenID)
+		if err != nil {
+			c.JSON(404, gin.H{"error": "token not found"})
+			return
+		}
+		alertText := "Fue activado el token " + strings.ToLower(token.Msg) + " desde la IP: " + c.ClientIP()
+		Alert(alertText, token.Chat)
+	})
 }
